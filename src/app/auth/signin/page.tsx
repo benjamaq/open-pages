20bda71ef9666a76882e1bc74cc22b2b4fb6d1e3
test dev <testdev@bens-MacBook-Pro.local>
2025-10-22 23:43:33 +0200

    Auth: ensure /auth/signup and /auth/signin pages exist and render AuthForm

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,3 +1,13 @@
+import AuthForm from '@/components/AuthForm'
+
+export const dynamic = 'force-dynamic'
+
+export default function SigninPage() {
+  return (
+    <AuthForm mode="signin" />
+  )
+}
+
 import AuthForm from '../../../components/AuthForm'
 
 export default function SignInPage() {
--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,3 +1,13 @@
+import AuthForm from '@/components/AuthForm'
+
+export const dynamic = 'force-dynamic'
+
+export default function SignupPage() {
+  return (
+    <AuthForm mode="signup" />
+  )
+}
+
 import AuthForm from '../../../components/AuthForm'
 
 export default function SignUpPage() {
