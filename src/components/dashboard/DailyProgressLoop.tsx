071148de305e6503bf4c8fb384b4a006a1dd7ea4
test dev <testdev@bens-MacBook-Pro-2.local>
2026-01-11 20:24:25 +0100

    Fix: import useMemo in DailyProgressLoop to resolve runtime ReferenceError

--- src/components/dashboard/DailyProgressLoop.tsx
+++ src/components/dashboard/DailyProgressLoop.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { StackCostCard } from '@/components/insights/StackCostCard'
 import { abbreviateSupplementName } from '@/lib/utils/abbreviate'
 
